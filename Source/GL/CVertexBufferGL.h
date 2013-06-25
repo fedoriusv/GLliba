357aa86f5b5e6fac42ba2b9511a67ab20bc8a282
Fred <fredysneg@rambler.ru>
2013-06-25 20:12:18 +0300

    +added vbo;

---
+++ Source/GL/CVertexBufferGL.h
@@ -0,0 +1,29 @@
+#ifndef _CVERTEXBUFFER_H_
+#define _CVERTEXBUFFER_H_
+
+#include "Types.h"
+
+namespace glliba
+{
+	struct SVertexData;
+
+	class CVertexBufferGL
+	{
+	private:
+
+		static void initBufferObject( const uint _target, uint& _bufferID, const uint _size, void* _data);
+		static void initVertexAttribPointer ( const uint _vertexAttrib, const uint _size );
+		static void updateBufferSubData( const uint _target, const uint _bufferID, const uint _size, void* _data );
+		
+		static void deleteBuffers( const uint _vertexID );
+
+	public:
+
+		static void initBufferObjects( SVertexData& _vertexData );
+		static void updateBufferObject( SVertexData& _vertexData );
+		
+		static void deleteBufferObjects( SVertexData& _vertexData );
+	};
+}
+
+#endif
